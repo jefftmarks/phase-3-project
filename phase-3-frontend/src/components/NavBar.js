2abc93c9fbd4d72bf5cb7d2107581780fee6e862
Jeff Marks <jeff.t.marks@gmail.com>
2022-09-15 14:09:36 -0400

    styled navbar

--- phase-3-frontend/src/components/NavBar.js
+++ phase-3-frontend/src/components/NavBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import './NavBar.css'
+import './NavBar.css';
+import { FiPlus } from "react-icons/fi";
 
 
 function NavBar({ activeUser, setActiveUser })  {
@@ -26,19 +27,32 @@ function NavBar({ activeUser, setActiveUser })  {
 	}
 
 	return (
-  	<div className="navbar-div">
-      <h2>navbar</h2> 
+  	<div>
 			{activeUser ? (
-				<>
-					<button onClick={handleLogout}>Log Out</button>
-					<Link to={`/user/${activeUser.username}`}><button>Profile</button></Link>
-					<Link to={"/"}><button>Feed</button></Link> 
-					<Link to={"/create-menu"}><button>Create Menu</button></Link>
-				</>	
+				<div className="navbar-div">
+					<div className="nav-item-container">
+						<div className="nav-pic"
+							style={{backgroundImage: `url(${activeUser.image_url})`}}
+						>
+						</div>
+						<Link to={`/user/${activeUser.username}`}>
+							<button className="nav-item">Profile</button>
+						</Link>
+						<Link to={"/"}>
+							<button className="nav-item">Feed</button>
+							</Link> 
+						<Link to={"/create-menu"}>
+							<button className="nav-item">
+								Create Menu <FiPlus size="15" style={{marginBottom: "-2.5"}} />
+							</button>
+						</Link>
+						<button className="nav-item" onClick={handleLogout}>Log Out</button>
+					</div>	
+				</div>
 			) : null}
     </div>
     
 	)
 }
 
-export default NavBar
+export default NavBar;
